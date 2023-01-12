bd3ab89138a29c3acaa410a15d363e4bc6bd6ff0
Nicholas Westerhausen <nwesterhausen@gmail.com>
2023-01-12 10:15:47 -0500

    chore: 🚀 pull in source of clippy-check

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,106 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as github from '@actions/github';
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+import * as input from './input';
+import { CheckRunner } from './check';
+import { Err, Ok, Result } from './result';
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+async function version(cmd: string, args?: string[]): Promise<string> {
+    args = args === undefined ? ['-V'] : [...args, '-V'];
+    return (await exec.getExecOutput(cmd, args, { silent: true })).stdout;
 }
 
-run()
+function prefix(prefix: string, options: string[]): string[] {
+    return options.flatMap(opt => [
+        prefix,
+        opt === 'warnings' ? opt : opt.startsWith('clippy::') ? opt : 'clippy::' + opt,
+    ]);
+}
+
+export async function run(actionInput: input.Input): Promise<Result<void, string>> {
+    const startedAt = new Date().toISOString();
+
+    let rustcVersion = await version('rustc');
+    let cargoVersion = await version('cargo');
+    let clippyVersion = await version('cargo', ['clippy']);
+
+    const warn = prefix('--warn', actionInput.warn);
+    const allow = prefix('--allow', actionInput.allow);
+    const deny = prefix('--deny', actionInput.deny);
+    const forbid = prefix('--forbid', actionInput.forbid);
+
+    let runner = new CheckRunner();
+    let stdErr = '';
+    let clippyExitCode = 0;
+    try {
+        core.startGroup('Executing cargo fmt (JSON output)');
+        const execOutput = await exec.getExecOutput(
+            'cargo',
+            [
+                'clippy',
+                '--message-format=json',
+                ...actionInput.options.filter(opt => !opt.startsWith('--message-format')),
+                '--',
+                ...warn,
+                ...allow,
+                ...deny,
+                ...forbid,
+            ],
+            {
+                ignoreReturnCode: true,
+                failOnStdErr: false,
+                listeners: {
+                    stdline: (line: string) => {
+                        runner.tryPush(line);
+                    },
+                },
+            },
+        );
+        stdErr = execOutput.stderr;
+        clippyExitCode = execOutput.exitCode;
+    } finally {
+        core.endGroup();
+    }
+
+    let sha = github.context.sha;
+    const pr = github.context.payload.pull_request;
+    if (pr !== undefined && 'head' in pr) {
+        sha = pr.head.sha;
+    }
+    await runner.executeCheck({
+        token: actionInput.token,
+        name: actionInput.name,
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        head_sha: sha,
+        started_at: startedAt,
+        context: {
+            rustc: rustcVersion,
+            cargo: cargoVersion,
+            clippy: clippyVersion,
+        },
+    });
+
+    if (clippyExitCode !== 0) {
+        if (
+            stdErr
+                .split('\n')
+                .map(line => line.startsWith('error: internal compiler error'))
+                .reduce((acc, ice) => acc || ice, false)
+        ) {
+            core.setOutput('Suppress ICEs', stdErr);
+            return new Ok(undefined);
+        }
+        return new Err(`Clippy had exited with the ${clippyExitCode} exit code:\n${stdErr}`);
+    }
+    return new Ok(undefined);
+}
+
+async function main(): Promise<void> {
+    const actionInput = input.get();
+    const res = await run(actionInput);
+    if (res.type === 'failure') core.setFailed(`${res.unwrap_err()}`);
+}
+
+main();
