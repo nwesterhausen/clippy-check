c91d6618fa85cb65d86f5ccb78839bd649900020
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2023-02-01 10:16:07 -0500

    perform linting

--- src/input.ts
+++ src/input.ts
@@ -1,33 +1,33 @@
-import stringArgv from 'string-argv';
-import * as core from '@actions/core';
+import * as core from '@actions/core'
+import stringArgv from 'string-argv'
 
 // Parsed action input
 export interface Input {
-    token: string;
-    options: string[];
-    warn: string[];
-    allow: string[];
-    deny: string[];
-    forbid: string[];
-    name: string;
+  token: string
+  options: string[]
+  warn: string[]
+  allow: string[]
+  deny: string[]
+  forbid: string[]
+  name: string
 }
 
 export function get(): Input {
-    const token = core.getInput('token', { required: true });
-    const options = stringArgv(core.getInput('options', { required: false }));
-    const warn = stringArgv(core.getInput('warn', { required: false }));
-    const allow = stringArgv(core.getInput('allow', { required: false }));
-    const deny = stringArgv(core.getInput('deny', { required: false }));
-    const forbid = stringArgv(core.getInput('forbid', { required: false }));
-    const name = core.getInput('name', { required: false });
+  const token = core.getInput('token', {required: true})
+  const options = stringArgv(core.getInput('options', {required: false}))
+  const warn = stringArgv(core.getInput('warn', {required: false}))
+  const allow = stringArgv(core.getInput('allow', {required: false}))
+  const deny = stringArgv(core.getInput('deny', {required: false}))
+  const forbid = stringArgv(core.getInput('forbid', {required: false}))
+  const name = core.getInput('name', {required: false})
 
-    return {
-        token,
-        options,
-        warn,
-        allow,
-        deny,
-        forbid,
-        name,
-    };
+  return {
+    token,
+    options,
+    warn,
+    allow,
+    deny,
+    forbid,
+    name
+  }
 }
